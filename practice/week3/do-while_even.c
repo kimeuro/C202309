1ad07816c9d3953d61bb0a7bb2fd2785035a4f65
kimeuro <kimeuro1129@naver.com>
2023-09-18 16:38:30 +0900

    Week3.1 반복문

---
+++ practice/week3/do-while_even.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int i = 0;  //i 초기화
+	do {
+		if (i % 2 == 0) {
+			i++;  //i가 만약 2라면 컨티뉴를 해도 계속 i는 2이기에 if문에 걸려 무한반복에 걸림. 따라서 i의 값을 증가시킴으로서 이를 해결함
+			continue;
+		}
+		printf("%d Hello World!\n", i++);
+	} while (i < 10);
+	return 0;
+}
