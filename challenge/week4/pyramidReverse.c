d9ccd1715d635b0638ad223044e8e358b6f9bb9e
kimeuro <kimeuro1129@naver.com>
2023-09-27 15:35:18 +0900

    Week4.2. 역피라미드 쌓기

---
+++ challenge/week4/pyramidReverse.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int floor;  //몇 층을 쌓을지 정하는 변수 선언
+	printf("몇 층을 쌓겠습니까? (5~100)");  //프린트문으로 묻고 스캔문으로 변수에 값을 넣음
+	scanf_s("%d", &floor);
+
+	for (int i = 0; i < floor; i++) {  //첫 번쨰 for문으로 전체적인 피라미드의 높이를 구현
+		for (int j = 0; j < i ; j++) {  //이중 for문으로 피라미드에 들어갈 공백과 별을 구현
+			printf("S");
+		}
+		for (int k = 0; k < 2*floor - (2*i + 1); k++) {  //11, 9, 7... 과 같이 홀수의 값만 나오도록 for문을 작성
+			printf("*");
+		}
+		printf("\n");  //피라미드 층을 구현한 다음에 다음 층수를 구현하기 위해서 넘어가게 함
+	}
+	return 0;
+}
