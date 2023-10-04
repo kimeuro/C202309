95bb678b536fbe572b90a52f62fc90084a62c771
kimeuro <kimeuro1129@naver.com>
2023-10-04 16:16:53 +0900

    Week5. 함수 개념

---
+++ challenge/week5/functionMulti.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+long multiF(int value);  // 함수 선언
+
+void main(void) {
+	printf("1부터 2까지의 곱은 %d\n", multiF(2));
+	printf("1부터 3까지의 곱은 %d\n", multiF(3));
+	printf("1부터 5까지의 곱은 %d\n", multiF(5));
+}
+
+long multiF(int value) {  // 함수 정의
+	int multi = 1;
+	for (int i = 1; i <= value; i++) {
+		multi = multi * i;  // multi 함수에 곱들의 연산을 쌓아감
+	}
+	return multi;  // 곱들의 연산을 반환함 
+}
---
+++ practice/week5/functionHap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int hapf(int value);  // hapf 라는 함수 선언
+
+void main(void) {
+	printf("1부터 10까지의 합은 %d\n", hapf(10));
+	printf("1부터 100까지의 합은 %d\n", hapf(100));
+	printf("1부터 1000까지의 합은 %d\n", hapf(1000));
+}
+
+int hapf(int value) {  // hapf 함수 정의
+	int i = 1;  // 선언과 초기화
+	int hap = 0;
+
+	while (i <= value) {
+		hap = hap + i;  // 1부터 value 값까지의 합을 hap 변수에 쌓아감
+		i++;
+	}
+	return hap;  // hapf 함수의 반환값을 hap으로 출력함
+}
